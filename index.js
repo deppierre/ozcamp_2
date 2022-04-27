f7b9434a842161405d09f9d1ea2f54e4836b746c
Pierre Depretz <pierre.depretz@mongodb.com>
2022-04-27 11:49:51 +1000

    stable version

--- index.js
+++ index.js
@@ -4,86 +4,110 @@ const fs = require("fs")
 const env = require('dotenv').config()
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { map } = require("cheerio/lib/api/traversing");
+const headers = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9,es;q=0.8"
+};
+
+//NP campings
+function getCampings(parks) {
+    for (const [key, value] of parks.entries()) {
+        request({
+            uri: value.link,
+            headers: headers,
+            gzip: true,
+        }, function (err, res, body) {
+            if (err) return console.error(err);
+            let $ = cheerio.load(body);
+
+            $(".scrollingBox__item.camping h3 a").get().map(ele => {
+                value.campings.push({
+                    name: $(ele).text(),
+                    link: $(ele).attr('href')
+                })
+                console.log(`getCampings::New camping added (${$(ele).text()})`)
+            });
+        });
+    }
+}
 
 //National Parks
-async function refreshParkList(client) {
-    var parks = [];
-    var mycoll = "parks";
-    const headers = {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "en-US,en;q=0.9,es;q=0.8"
-    };
-
-    var response = await request({
+function getNationalParks(callback) {
+    request({
         uri: process.env.PARKS,
         headers: headers,
         gzip: true,
-    });
+    }, function (err, res, body) {
+        if (err) return console.error(err);
 
-    var $ = cheerio.load(response);
-
-    $(".dynamicListing li a").each(function (idx, el) {
-        var ParkName = $(el).text();
-        var link = $(el).attr('href');
-    
-        parks.push(
-            { 
-                ParkName: ParkName,
-                type: "National Park",
-                "url": link,
-                "campings": []
+        let $ = cheerio.load(body);    
+        let parks = $(".dynamicListing li a").get().map(ele => {
+            const link = $(ele).attr('href');
+            return {
+                ParkName: $(ele).text(),
+                link: link,
+                campings: []
             }
-        );
-    });
-
-    //Camping URL
-    parks.forEach(async function (mypark, index, arr) {
-        var response = await request({
-            uri: mypark.url,
-            headers: headers,
-            gzip: true,
         });
-
-        var $ = cheerio.load(response);
-
-        $(".scrollingBox__item a").each(async function (idx, el) {
-            var link = $(el).attr('href');
-            await parks[index].campings.push(link);
-        });
-        console.log(parks);
+        callback(parks);
     });
-        // $(".scrollingBox__item").each(function (idx, el) {
-        //     var name = $(el).text();
-        //     var link = $(el).attr('href');
+};
 
-        //     console.log(`${name} ${link}`);
-        // });  
-
-    try{
-        await client.db().dropCollection(mycoll);
-        console.log(`refreshParkList::Collection ${mycoll} deleted`);
-    } catch(err) {
-        if (err.codeName == 'NamespaceNotFound') {
-            console.log(`refreshParkList::Collection ${mycoll} already deleted`)
-        }
-    } finally{
-        let result = await client.db().collection(mycoll).insertMany(parks);
-        console.log(`refreshParkList::${result.insertedCount} parks created`);
-    }
-}
-
-async function main() {    
+function insertMany(documents){
     //MongoDB
     const uri = process.env.URI;
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-    await client.connect();
+    const mycoll = "parks";
 
-    try {
-        await refreshParkList(client);
-    } finally {
-        await client.close();
-    }
+    client.connect( async function(err, db) {
+        if (err) throw err;
+
+        try{
+            await client.db().dropCollection(mycoll);
+            console.log(`refreshParkList::Collection ${mycoll} deleted`);
+        } catch(err) {
+            if (err.codeName == 'NamespaceNotFound') {
+                console.log(`refreshParkList::Collection ${mycoll} already deleted`)
+            }
+        } finally{
+            let result = await client.db().collection(mycoll).insertMany(parks);
+            console.log(`refreshParkList::${result.insertedCount} parks created`);
+            client.close();
+        }
+    });
 }
+//     //Camping URL
+//     parks.forEach(async function (mypark, index, arr) {
+//         var response = await request({
+//             uri: mypark.url,
+//             headers: headers,
+//             gzip: true,
+//         });
 
-main().catch(console.error);
+//         var $ = cheerio.load(response);
+
+
+//         // $(".scrollingBox__item").each(function (idx, el) {
+//         //     var name = $(el).text();
+//         //     var link = $(el).attr('href');
+
+//         //     console.log(`${name} ${link}`);
+//         // });  
+
+//     try{
+//         await client.db().dropCollection(mycoll);
+//         console.log(`refreshParkList::Collection ${mycoll} deleted`);
+//     } catch(err) {
+//         if (err.codeName == 'NamespaceNotFound') {
+//             console.log(`refreshParkList::Collection ${mycoll} already deleted`)
+//         }
+//     } finally{
+//         let result = await client.db().collection(mycoll).insertMany(parks);
+//         console.log(`refreshParkList::${result.insertedCount} parks created`);
+//     }
+// }
+
+getNationalParks(getCampings);
+// getCampings(insertMany);
