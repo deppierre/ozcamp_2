0b25359a1758753aef5adb7be964932ddc37004f
Pierre Depretz <pierre.depretz@mongodb.com>
2022-07-02 18:47:20 +1000

    update

--- index.js
+++ index.js
@@ -18,29 +18,19 @@ function getExecutionTime(){
     }, simulateTime)
 }
 
-// async function insertMany(documents){
-//     //MongoDB
-//     const uri = process.env.URI;
-//     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-//     const mycoll = "parks";
-
-//     client.connect( async function(err, db) {
-//         if (err) throw err;
-
-//         try {
-//             await client.db().dropCollection(mycoll);
-//             console.log(`insertMany:: Collection ${mycoll} deleted`);
-//         } catch(err) {
-//             if (err.codeName == 'NamespaceNotFound') {
-//                 console.log(`insertMany:: Collection ${mycoll} already deleted`)
-//             }
-//         } finally {
-//             const result = await client.db().collection(mycoll).insertMany(documents);
-//             console.log(`insertMany:: ${result.insertedCount} documents created`);
-//             client.close();
-//         }
-//     });
-// }
+async function insertMany(documents, client, mycoll=""){
+    try {
+        await client.db().dropCollection(mycoll);
+        console.log(`INFO: Collection ${mycoll} deleted`);
+    } catch(err) {
+        if (err.codeName == 'NamespaceNotFound') {
+            console.log(`WARNING: Collection ${mycoll} already deleted`)
+        }
+    } finally {
+        const result = await client.db().collection(mycoll).insertMany(documents);
+        return result
+    }
+}
 
 function getHtmlBody(url){
     if(url){
@@ -53,34 +43,49 @@ function getHtmlBody(url){
 }
 
 async function getNationalParks() {
-    try{
-        const allelements = {}
-        const body1 = await getHtmlBody(process.env.PARKS)
+    const allelements = {}
+    const body1 = await getHtmlBody(process.env.PARKS)
 
-        for (const element of body1(".dynamicListing li a")) {
-            allelements[body1(element).text().trim()] = {
-                url: body1(element).attr('href')
+    for (const element of body1(".dynamicListing li a")) {
+        allelements[body1(element).text().trim()] = {
+            url: body1(element).attr('href')
+        }
+    }
+
+    await Promise.all(Object.entries(allelements).map(async([key, value]) => {
+        const body2 = await getHtmlBody(value.url)
+        const newBody2 = body2(".scrollingBox__item.camping h3 a")
+        if(newBody2.length > 0){
+            allelements[key]["campings"] = {}
+            for (const element2 of newBody2){
+                allelements[key]["campings"][body2(element2).text().trim()] = value.url.split("/")[2] + body2(element2).attr('href').trim()
             }
         }
-        await Promise.all(Object.entries(allelements).map(async([key, value]) => {
-            const body2 = await getHtmlBody(value.url)
-            const newBody2 = body2(".scrollingBox__item.camping h3 a")
-            if(newBody2.length > 0){
-                allelements[key]["campings"] = {}
-                for (const element2 of newBody2){
-                    allelements[key]["campings"][body2(element2).text().trim()] = value.url.split("/")[2] + body2(element2).attr('href').trim()
-                }
-            }
-        }))
-        console.log(`INFO: ${Object.keys(allelements).length} parks fetched`)
-    } catch(err) {
-        console.error(`ERROR: ${err}`)
-    }
+    }))
+    return allelements
 };
 
 async function main () {
-    await getNationalParks();
-    getExecutionTime();
+    try{
+        //MongoDB
+        const uri = process.env.URI;
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+        client.connect( async (err, db) => {
+            if (err) throw err;
+        })
+
+        const parks = await getNationalParks();
+        console.log(`INFO: ${Object.keys(parks).length} parks fetched`)
+        const insertParks = await insertMany(parks, mycoll="parks", client);
+        console.log(`INFO: ${insertParks.insertedCount} documents created`);
+    }
+    catch(err) {
+        console.error(`ERROR: ${err}`)
+    }
+    finally{
+        getExecutionTime();
+    }
 }
 
 main()
