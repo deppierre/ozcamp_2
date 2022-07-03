65895aa08fc5060bb61ca0c6d1291de83bb9e4f0
Pierre Depretz <pierre.depretz@mongodb.com>
2022-07-03 18:56:29 +1000

    update

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const cheerio = require("cheerio")
+const mongoose = require("mongoose")
 const axios = require("axios")
 const env = require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const { data } = require("cheerio/lib/api/attributes");
 
-const start = new Date()
-const hrstart = process.hrtime()
-const simulateTime = 5
-
-function getExecutionTime(){
-    setTimeout(function (argument) {
-        // execution time simulated with setTimeout function
-        const end = new Date() - start,
-        hrend = process.hrtime(hrstart)
-    
-        console.info('INFO: Execution time: %dms', end)
-    }, simulateTime)
-}
-
-async function insertMany(documents, client, mycoll=""){
+async function insertMany(documents, mycoll=""){
     try {
         await client.db().dropCollection(mycoll);
         console.log(`INFO: Collection ${mycoll} deleted`);
@@ -43,49 +30,53 @@ function getHtmlBody(url){
 }
 
 async function getNationalParks() {
-    const allelements = {}
+    const 
+        allpromises = []
+        allelements = []
     const body1 = await getHtmlBody(process.env.PARKS)
 
     for (const element of body1(".dynamicListing li a")) {
-        allelements[body1(element).text().trim()] = {
-            url: body1(element).attr('href')
+        const site = {
+            "name": body1(element).text().trim(),
+            "url": body1(element).attr('href')
         }
+        allelements.push(site)
     }
 
-    await Promise.all(Object.entries(allelements).map(async([key, value]) => {
-        const body2 = await getHtmlBody(value.url)
-        const newBody2 = body2(".scrollingBox__item.camping h3 a")
-        if(newBody2.length > 0){
-            allelements[key]["campings"] = {}
-            for (const element2 of newBody2){
-                allelements[key]["campings"][body2(element2).text().trim()] = value.url.split("/")[2] + body2(element2).attr('href').trim()
+    await Promise.all(allelements.map(async (value, key) => {
+        return getHtmlBody(value.url).then( (body2) => {
+            const newBody2 = body2(".scrollingBox__item.camping h3 a")
+            if(newBody2.length > 0){
+                allelements[key]["campings"] = {}
+                for (const element2 of newBody2){
+                    allelements[key]["campings"][body2(element2).text().trim()] = value.url.split("/")[2] + body2(element2).attr('href').trim()
+                }
             }
-        }
+        })
     }))
     return allelements
 };
 
 async function main () {
     try{
+        console.time("Execution time");
         //MongoDB
-        const uri = process.env.URI;
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-        client.connect( async (err, db) => {
-            if (err) throw err;
-        })
+        await client.connect();
 
         const parks = await getNationalParks();
         console.log(`INFO: ${Object.keys(parks).length} parks fetched`)
-        const insertParks = await insertMany(parks, mycoll="parks", client);
+        const insertParks = await insertMany(parks, mycoll="parks");
         console.log(`INFO: ${insertParks.insertedCount} documents created`);
     }
     catch(err) {
         console.error(`ERROR: ${err}`)
     }
     finally{
-        getExecutionTime();
+        await client.close();
+        console.timeEnd("Execution time");
     }
 }
 
-main()
+const client = new MongoClient(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+main();
